a4c62c12a959fb0a7da710d0a68ffab0416ade51
Jakob Lindskog <jakob.lindskog@gmail.com>
2022-01-03 21:43:54 +0100

    lock config to Noto Sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': '"Noto Sans Display",ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"'
+    },
     extend: {
       width: {
         'fhd': '1920px',
