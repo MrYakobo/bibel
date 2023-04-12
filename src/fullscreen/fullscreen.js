c3d7567e90dc134cd1b5047c8dd56310da5f6a30
Jakob Lindskog <jakob.lindskog@gmail.com>
2023-04-12 17:44:19 +0200

    also fix fullscreen

--- src/fullscreen/fullscreen.js
+++ src/fullscreen/fullscreen.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import Fullscreen from './Fullscreen.vue';
 import 'virtual:windi.css'
 
+import store from '../store'
+
 new Vue({
     render: (h) => h(Fullscreen),
+    store
 }).$mount('#fullscreen');
