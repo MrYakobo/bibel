332e3d11bcd50ebf5711380ee43f6e39548dea89
Jakob Lindskog <jakob.lindskog@gmail.com>
2023-06-18 20:37:28 +0200

    fix deadly bug in paginate, that removed the last sentence

--- src/paginate.js
+++ src/paginate.js
@@ -9,12 +9,13 @@ function paginate(longText, maxChunkSize) {
         // if we are already mogen, accept it
         let chunk_is_mogen = chunk.length + sentence.length + 1 >= maxChunkSize
         if (chunk_is_mogen && chunk.length > 0) {
+            // push the chunk and empty our local acc variable.
             chunks.push(chunk)
             chunk = ""
-            continue
         }
 
-        // we are not mogen. build on the chunk
+        // we are either mogen and have an empty chunk here, OR
+        // we are not mogen. either way, build on the chunk; so that no sentence gets lost
         chunk = chunk + sentence + " "
     }
 
