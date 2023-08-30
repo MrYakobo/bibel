b22b2d0240c45409c8b4100e300869ab197b1c43
Jakob Lindskog <jakob.lindskog@gmail.com>
2023-08-30 10:26:28 +0200

    rm superflous ping info

--- src/components/KeysViz.vue
+++ src/components/KeysViz.vue
@@ -19,17 +19,11 @@
             <span class="text-2xl" v-html="kbd.label"></span>
             <p class="text-sm">{{ kbd.text }}</p>
         </div>
-        <div class="text-lg flex flex-col justify-center ml-3">
-            <p>⬆️ {{Math.round(write_ping,2)}}ms</p>
-            <p>️⬇ {{Math.round(read_ping,2)}}ms</p>
-        </div>
     </div>
 </template>
 <script>
-import {mapState} from 'vuex'
 export default {
     name: "KeysViz",
     props: ['keyboard_mappings'],
-    computed: mapState(["read_ping", "write_ping"])
 }
 </script>
